def4b58a5d0d854a90712ae3356d4808bc72b031
Anwesh43 <anweshthecool0@gmail.com>
2019-03-09 16:44:30 +0530

    adding LineDortExpERot class

--- linedotexpview/src/main/java/com/anwesh/uiprojects/linedotexpview/LineDotExpRotView.kt
+++ linedotexpview/src/main/java/com/anwesh/uiprojects/linedotexpview/LineDotExpRotView.kt
@@ -158,4 +158,28 @@ class LineDotExpRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineDotExpRot(var i : Int) {
+
+        private val root : LDERNode = LDERNode(0)
+        private var curr : LDERNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
