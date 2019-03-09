a4d8416f21a846b80549d6a57d8011c9e9bb90e8
Anwesh43 <anweshthecool0@gmail.com>
2019-03-09 16:22:42 +0530

    adding LineDotExpRotView class

--- linedotexpview/src/main/java/com/anwesh/uiprojects/linedotexpview/LineDotExpRotView.kt
+++ linedotexpview/src/main/java/com/anwesh/uiprojects/linedotexpview/LineDotExpRotView.kt
@@ -49,3 +49,21 @@ fun Canvas.drawLDERNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class LineDotExpRotView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
