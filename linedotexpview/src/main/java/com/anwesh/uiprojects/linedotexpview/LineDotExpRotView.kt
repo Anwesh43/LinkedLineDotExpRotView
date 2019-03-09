ab647670b32ec90b4045fa7af29d00ff99c85299
Anwesh43 <anweshthecool0@gmail.com>
2019-03-09 16:36:52 +0530

    adding LDERNode class

--- linedotexpview/src/main/java/com/anwesh/uiprojects/linedotexpview/LineDotExpRotView.kt
+++ linedotexpview/src/main/java/com/anwesh/uiprojects/linedotexpview/LineDotExpRotView.kt
@@ -114,4 +114,48 @@ class LineDotExpRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LDERNode(var i : Int, val state : State = State()) {
+
+        private var next : LDERNode? = null
+        private var prev : LDERNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LDERNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLDERNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LDERNode {
+            var curr : LDERNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
